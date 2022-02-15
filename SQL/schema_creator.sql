fa1a983e674c88f71fcf2b38cb18fc688b5928dd
Eleo <eleonorajmartin@Eleos-Air.attlocal.net>
2022-02-14 20:15:39 -0800

    Pushing multiple changes and files

---
+++ SQL/schema_creator.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE transfers (
+    name VARCHAR(60)   NOT NULL,
+    position VARCHAR(30)   NOT NULL,
+    age INT   NOT NULL,
+    team_from VARCHAR(30)   NOT NULL,
+    league_from VARCHAR(30)   NOT NULL,
+    team_to VARCHAR(30)   NOT NULL,
+    league_to VARCHAR(30)   NOT NULL,
+    season VARCHAR(30)   NOT NULL,
+    market_value INT   NOT NULL,
+    transfer_fee INT   NOT NULL,
+    CONSTRAINT pk_transfers PRIMARY KEY (
+        name
+     )
+);
+
+CREATE TABLE league_countries (
+    league_name VARCHAR(30)   NOT NULL,
+    country VARCHAR(30)   NOT NULL,
+    CONSTRAINT pk_league_countries PRIMARY KEY (
+        league_name
+     )
+);
+
+CREATE TABLE locations (
+    country VARCHAR(30)   NOT NULL,
+    country_code VARCHAR(10)   NOT NULL,
+    latitude VARCHAR(30)   NOT NULL,
+    longitude VARCHAR(30)   NOT NULL,
+    CONSTRAINT pk_locations PRIMARY KEY (
+        country
+     )
+);
+
+ALTER TABLE transfers ADD CONSTRAINT fk_transfers_league_from FOREIGN KEY(league_from)
+REFERENCES league_countries (league_name);
+
+ALTER TABLE transfers ADD CONSTRAINT fk_transfers_league_to FOREIGN KEY(league_to)
+REFERENCES league_countries (league_name);
+
+ALTER TABLE league_countries ADD CONSTRAINT fk_league_countries_country FOREIGN KEY(country)
+REFERENCES locations (country);
