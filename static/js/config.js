43b48eb356a816407804f6d4d8d6965a3064f2ee
Kevin  Perez <kevinperez@kevins-mbp.lan>
2022-02-25 21:40:03 -0800

    updated code for maps

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,4 @@
 // API key
 const API_KEY = "pk.eyJ1Ijoia2V2aW5rdm5wciIsImEiOiJja3pwMzE4emMyajl4MnBzOGFsdzFqMzFpIn0.O-GB2-Voj6EjnBqL9Kex8w";
+
+const API_KEY2 = "pk.eyJ1Ijoia2V2aW5rdm5wciIsImEiOiJja3pwMzE4emMyajl4MnBzOGFsdzFqMzFpIn0.O-GB2-Voj6EjnBqL9Kex8w"
